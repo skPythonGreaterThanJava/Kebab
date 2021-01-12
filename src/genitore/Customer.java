f2f47b6386c5c010069bf951c193df8c84135870
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2021-01-12 13:12:11 +0100

    the Program

---
+++ src/genitore/Customer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genitore;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sesana.federico
+ */
+public class Customer implements Runnable{
+    private boolean satisfied = false, stop = false;
+    private Kebabbaro kebabbaro;
+    int num;
+    
+    public Customer(Kebabbaro kebabbaro, int num) {
+        this.kebabbaro = kebabbaro;
+        this.num = num;
+    }
+    
+    @Override
+    public void run() {
+        while(!stop){
+            while(!satisfied){
+                try {
+                    satisfied = kebabbaro.kebabbing();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Customer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if(satisfied)
+                    System.out.println("Giovanni soddisfatto: " + num);
+            }
+        }
+    }
+    
+    public void end() {
+        stop = true;
+    }
+}
